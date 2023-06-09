27e0d23240485416b71fd841253e9530ed7c6948
yca202 <124802235+yca202@users.noreply.github.com>
2023-06-09 09:31:30 +0100

    Update quiz.js

--- main/js/quiz.js
+++ main/js/quiz.js
@@ -331,7 +331,7 @@ function startTimerLine(time) {
   counterLine = setInterval(timer, 29);
   function timer() {
     time += 1; //upgrading time value with 1
-    time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+    time_line.style.width = (time / 550) * 100 + "%"; //increasing width of time_line with px by time value
     if (time > 549) {
       //if time value is greater than 549
       clearInterval(counterLine); //clear counterLine
